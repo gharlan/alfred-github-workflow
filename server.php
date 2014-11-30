e7180709f81349682c90516f0ea2f2f0ee964853
Gregor Harlan <mail@gh01.de>
2014-11-30 16:00:43 +0100

    server page with styling

--- server.php
+++ server.php
@@ -1,5 +1,9 @@
 <?php
 
+if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER['REQUEST_URI'])) {
+    return false;
+}
+
 require 'workflow.php';
 
 Workflow::init();
@@ -11,4 +15,51 @@ if (!isset($_GET['access_token'])) {
 
 Workflow::setConfig('access_token', $_GET['access_token']);
 
-echo 'alfred-github-workflow is ready. Have fun.';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>alfred-github-workflow</title>
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+            }
+            body {
+                background: #999;
+                font-family: 'Helvetica Neue', sans-serif;
+                font-size: 14pt;
+            }
+            div {
+                padding-top: 200px;
+                width: 450px;
+                margin: 0 auto;
+            }
+            img {
+                display: block;
+                float: left;
+                margin-right: 20px;
+            }
+            p {
+                padding-top: 20px;
+            }
+            span {
+                background: #444;
+                color: #fff;
+                padding: 5px;
+                margin-bottom: 5px;
+                display: inline-block;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <img src="icon.png" width="125" height="125"/>
+            <p>
+                <span>alfred-github-workflow is ready.</span><br>
+                <span>Have fun.</span>
+            </p>
+        </div>
+    </body>
+</html>

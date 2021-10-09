081238f7c9db4cf86356e598abdfb500e846b9cc
atsumi <atsumi.tatsuya@gmail.com>
2021-10-09 22:32:30 +0900

    Update search.php
    Co-authored-by: Gregor Harlan <mail@gh01.de>

--- search.php
+++ search.php
@@ -422,7 +422,7 @@ class Search
                 'wiki' => array('Pull up the wiki'),
                 'commits' => array('View commit history'),
                 'releases' => array('See latest releases'),
-                'actions' => array('Show Github Actions', 'actions'),
+                'actions' => array('Show Github Actions'),
             );
             foreach ($subs as $key => $sub) {
                 Workflow::addItem(Item::create()

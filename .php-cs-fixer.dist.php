dd8196210491a792a290e006323e11b044da0ae5
Gregor Harlan <mail@gh01.de>
2024-01-28 14:27:08 +0100

    update cs fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,7 +7,9 @@ return (new PhpCsFixer\Config())
         '@Symfony:risky' => true,
         'empty_loop_body' => ['style' => 'semicolon'],
         'method_argument_space' => false,
+        'modernize_strpos' => false,
         'native_constant_invocation' => false,
+        'no_superfluous_phpdoc_tags' => false,
         'psr_autoloading' => false,
         'no_useless_else' => true,
     ])
